5dd517efc555cc3f5e8f70fa1d7c3b65f6d3af30
Mcorduk <om.corduk@gmail.com>
2024-01-12 21:22:59 +0300

    feat: make cart component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,20 @@
+import { NavLink } from "react-router-dom";
 import styles from "./Cart.module.css";
 import ShoppingCartImage from "/public/cart.png";
 
 export default function Cart({ cartItems, setCartItems }) {
   return (
-    // Inserting the cart Png in Nav here by directly importing it
-    <img className={styles.cart} src={ShoppingCartImage} alt="Shopping Cart" />
+    <NavLink
+      className={(navData) =>
+        navData.isActive ? "active-link" : "non-active-link"
+      }
+      to="/shopping-cart"
+    >
+      <img
+        className={styles.cart}
+        src={ShoppingCartImage}
+        alt="Shopping Cart"
+      />
+    </NavLink>
   );
 }
