2d2e49879c492652edd1f8d67625d7c57103a7f0
Mcorduk <om.corduk@gmail.com>
2024-01-12 16:57:39 +0300

    feat: addhtml and styling to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,27 @@
 import Header from "../components/Header/Header";
-
+import "./Home.module.css";
 export default function Home() {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <header>
+          <h1>
+            Discover Elegance <br />
+            in Every Detail
+          </h1>
+        </header>
+        <section>
+          {/* <p>
+            Welcome to <strong>NobleNest Living</strong>, where quality meets
+            style. Our curated collection of handcrafted furniture pieces is
+            designed to transform your space into a haven of comfort and
+            sophistication. From timeless classics to modern marvels, explore
+            our range and elevate your home with furniture that reflects your
+            unique taste.
+          </p> */}
+        </section>
+      </main>
     </>
   );
 }
