d94d79a2c231320da5a0a2dc6194266d3056f1ce
Mcorduk <om.corduk@gmail.com>
2024-01-12 22:29:26 +0300

    fix: recreate router to fix navbar bug

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import ErrorPage from "../pages/ErrorPage";
 import Home from "../pages/Home/Home";
 import KidsRoom from "../pages/Rooms/KidsRoom";
@@ -6,42 +6,17 @@ import Kitchen from "../pages/Rooms/Kitchen";
 import LivingRoom from "../pages/Rooms/LivingRoom";
 import ShoppingCart from "../pages/ShoppingCart";
 
-const Router = (props) => {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-      label: "Home",
-      exact: true,
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/living-room",
-      element: <LivingRoom {...props} />,
-      label: "Living Room",
-      exact: true,
-    },
-    {
-      path: "/kitchen",
-      element: <Kitchen {...props} />,
-      label: "Kitchen",
-      exact: true,
-    },
-    {
-      path: "/kids-room",
-      element: <KidsRoom {...props} />,
-      label: "Kid's Room",
-      exact: true,
-    },
-    {
-      path: "/shopping-cart",
-      element: <ShoppingCart {...props} />,
-      label: "Shopping Cart",
-      exact: true,
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
+const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/living-room" element={<LivingRoom />} />
+      <Route path="/kitchen" element={<Kitchen />} />
+      <Route path="/kids-room" element={<KidsRoom />} />
+      <Route path="/shopping-cart" element={<ShoppingCart />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  );
 };
 
 export default Router;
