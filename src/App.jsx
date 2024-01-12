2277a8777cb8e31b8217b73ec0de262b5f7e9f81
Mcorduk <om.corduk@gmail.com>
2024-01-12 14:18:52 +0300

    feat: add css reset and style body to add a background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
-import Home from "./pages/Home";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import About from "./pages/About";
+import Home from "./pages/Home";
 
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
