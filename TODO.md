c163fc94fa135abce23b9b07247ff2cb517c9552
Mcorduk <om.corduk@gmail.com>
2024-01-13 18:00:03 +0300

    feat: add form for quantity of items number input and add to cart button on each productCard

--- TODO.md
+++ TODO.md
@@ -1,11 +1,7 @@
 # TODO
 
-- Cart Items, are an array of Objects:
-  Item = {
-  - name
-  - price
-  - quantity
-    }
+- Build individual card elements for each of your products. Display an input field on it, which lets a user manually type in how many items they want to buy. Also, add an increment and decrement button next to it for fine-tuning. You can also display a title for each product as well as an “Add To Cart” button.
+
 - Since Items are an array of objects
   update the add item function to:
 
--- src/components/Products/ProductCard/ProductCard.jsx
+++ src/components/Products/ProductCard/ProductCard.jsx
@@ -1,21 +1,44 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 import styles from "./ProductCard.module.css";
 import useProductInfo from "/src/hooks/useProductInfo";
 
 export default function ProductCard({ productId }) {
   const { data, error, loading } = useProductInfo(productId);
 
+  // State for managing the quantity input value
+  const [quantity, setQuantity] = useState(1);
+
+  const handleQuantityChange = (event) => {
+    setQuantity(Number(event.target.value));
+  };
+
   if (error) return <p>An error occurred: {error}</p>;
   if (loading) return <p>Loading...</p>;
 
   return (
-    <article className={styles.wrapper}>
+    <figure className={styles.wrapper}>
       <img src={data.images[0]} />
-      <section>
+      <figcaption>
         <p>{data.title}</p>
         <p>{data.price}$</p>
+      </figcaption>
+      <section>
+        <form action="">
+          <button>Add to Cart</button>
+          <label htmlFor="product-quantity"></label>
+          <input
+            id="product-quantity"
+            type="number"
+            name="product-quantity"
+            onChange={handleQuantityChange}
+            value={quantity}
+            min={1}
+            max={10}
+          />
+        </form>
       </section>
-    </article>
+    </figure>
   );
 }
 
