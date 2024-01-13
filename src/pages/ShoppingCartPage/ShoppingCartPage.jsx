6692913ce84f1c87a1b7b96a55a51d2bee826744
Mcorduk <om.corduk@gmail.com>
2024-01-13 22:45:05 +0300

    feat: add and style a paragraph to render in case cart is empty

--- src/pages/ShoppingCartPage/ShoppingCartPage.jsx
+++ src/pages/ShoppingCartPage/ShoppingCartPage.jsx
@@ -1,25 +1,32 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import CartItemCard from "../../components/Cart/CartItemCard/CartItemCard";
 import styles from "./ShoppingCartPage.module.css";
 
 export default function ShoppingCart(props) {
   const cartItemArray = Object.values(props.cartItems);
-  console.log(cartItemArray);
   return (
     <main>
       <header>
         <h2>Your Cart</h2>
         <hr />
       </header>
-      <section className={styles.wrapper}>
-        {cartItemArray.map((cartItem) => (
-          <CartItemCard
-            key={cartItem.id}
-            cartItem={cartItem}
-            addItemToCart={props.addItemToCart}
-          />
-        ))}
-      </section>
+
+      {cartItemArray.length > 0 ? (
+        <section className={styles.wrapper}>
+          {cartItemArray.map((cartItem) => (
+            <CartItemCard
+              key={cartItem.id}
+              cartItem={cartItem}
+              addItemToCart={props.addItemToCart}
+            />
+          ))}
+        </section>
+      ) : (
+        <p style={{ fontSize: "1.6rem" }}>
+          Your cart is empty. <Link to="/living-room">Keep Shopping</Link>
+        </p>
+      )}
     </main>
   );
 }
