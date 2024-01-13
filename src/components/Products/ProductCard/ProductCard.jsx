2103b376a96c17343c883c3a93298e505231d6b1
Mcorduk <om.corduk@gmail.com>
2024-01-13 16:19:07 +0300

    feat: create custom hook for API fetching product information, integrated it into productCard component

--- src/components/Products/ProductCard/ProductCard.jsx
+++ src/components/Products/ProductCard/ProductCard.jsx
@@ -1,5 +1,23 @@
+import PropTypes from "prop-types";
+import useProductInfo from "../../hooks/useProductInfo";
 import styles from "./ProductCard.module.css";
 
-export default function ProductCard() {
-  return;
+export default function ProductCard({ productId }) {
+  const { data, error, loading } = useProductInfo(productId);
+
+  if (error) return <p>An error occurred: {error}</p>;
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <article>
+      <img src={data.images[0]} />
+      <section>
+        <p>{data.title}</p>
+      </section>
+    </article>
+  );
 }
+
+ProductCard.propTypes = {
+  productId: PropTypes.int.isRequired,
+};
---
+++ src/hooks/useProductInfo.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+const useProductInfo = (productId) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // API Used: https://fakeapi.platzi.com/en/about/introduction/
+
+    async function getData(productId) {
+      const URL = `https://api.escuelajs.co/api/v1/products/${productId}`;
+
+      try {
+        const response = await fetch(URL, { mode: "cors" });
+
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`,
+          );
+        }
+        let result = await response.json();
+        setData(result);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    if (productId) {
+      getData(productId);
+    }
+  }, [productId]); // Add productId to the dependency array
+
+  return { data, error, loading }; // Fix the variable name to match the state
+};
+
+export default useProductInfo;
